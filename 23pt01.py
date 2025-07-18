b0d48077ceeb3956e25af3436716f0d4164d395c
Aakash <aakashvpsgtech@gmail.com>
2025-07-18 12:45:55 +0530

    feat : milestone 3 done

--- 23pt01.py
+++ 23pt01.py
@@ -1,3 +1,4 @@
+import datetime 
 
 logfile = []
 with open('./Milestone1/M1_LogFile.txt', 'r') as f:
@@ -75,17 +76,116 @@ with open('./Milestone2/M2_CommandFile.txt', 'r') as f:
         line = line.rstrip('\n')
         splitline = line.split(' ', 1)
         # print(splitline)
-        print(splitline)
         if(splitline[0] == 'INSERT'):
-            break
-            # insertline[0] = '[' + insertline[0] + ']'
-            # print(insertline)
-            # logfile.insert(int(splitline[1])-1, insertline)
-            # print(logfile[int(splitline[1])-1])
+            timesplit = splitline[1].split(']')[0]
+            timesplit = timesplit.lstrip('[')
+            # print(timesplit)
+            i = 0
+            format_string = "%Y-%m-%d %H:%M:%S"
+            for log in logfile:
+                # print(log)
+                x = log[0].lstrip('[')
+                x = x.rstrip(']')
+                # print(x)
+                # print(timesplit)
+                # print(log[0])
+                logdate =  datetime.datetime.strptime(x, format_string)
+                insertdate = datetime.datetime.strptime(timesplit, format_string)
+                if(logdate < insertdate):
+                    break
+                else:
+                    i += 1
+            splitline = line.split(']')
+            splitline[0] = splitline[0].split(' ', 1)[1]
+            # print(splitline[0])
+            splitline[0] = splitline[0] + "]"
+            splitline[1] = splitline[1].lstrip()
+            splitline[1] = splitline[1].rstrip('\n')
+            # print(splitline)
+            logfile.insert(i, splitline)
+            # print(logfile[i])
         elif(splitline[0] == 'DELETE'):
             del logfile[int(splitline[1])-1]
 # print(logfile)
 
-# with open('./Milestone2/M2_ProcessedLogFile.txt', 'w') as f:
-#     for log in logfile:
-#         f.write(" ".join(log) + "\n")
+counts = {}
+for log in logfile:
+    if log[1].count('logged in'):
+        start = log[1].find("'")
+        end = log[1].find("'", start + 1)
+        username = log[1][start+1:end]
+        # print(username)
+        if username in counts:
+            counts[username] += 1
+        else:
+            counts[username] = 1
+
+with open('./Milestone2/M2_ProcessedLogFile.txt', 'w') as f:
+    for log in logfile:
+        f.write(" ".join(log) + "\n")
+
+with open('./Milestone2/M2_Output.txt', 'w') as f:
+    for username, count in counts.items():
+        f.write(f"{username}: {count}\n")
+
+logfile1 = []
+logfile2 = []
+
+with open('./Milestone3/M3_LogFile1.txt', 'r') as f:
+    content = f.readlines()
+    for line in content:
+        splitline = line.split(']')
+        splitline[0] = splitline[0] + "]"
+        splitline[1] = splitline[1].lstrip()
+        splitline[1] = splitline[1].rstrip('\n')
+        logfile1.append(splitline)
+
+with open('./Milestone3/M3_LogFile2.txt', 'r') as f:
+    content = f.readlines()
+    for line in content:
+        splitline = line.split(']')
+        splitline[0] = splitline[0] + "]"
+        splitline[1] = splitline[1].lstrip()
+        splitline[1] = splitline[1].rstrip('\n')
+        logfile2.append(splitline)
+
+commands = []
+i = 0
+j = 0
+format_string = "%Y-%m-%d %H:%M:%S"
+
+while i < len(logfile1) and j < len(logfile2):
+    log1_time = logfile1[i][0].lstrip('[').rstrip(']')
+    log2_time = logfile2[j][0].lstrip('[').rstrip(']')
+    log1_date = datetime.datetime.strptime(log1_time, format_string)
+    log2_date = datetime.datetime.strptime(log2_time, format_string)
+    logfile2[j][1] = logfile2[j][1].replace('  ', ' ')
+    if logfile1[i] == logfile2[j]:
+        i += 1
+        j += 1
+    else:
+        if log1_date == log2_date:
+            commands.append(f"REPLACE {i+1} {logfile2[j][1]}")
+            i += 1
+            j += 1
+        elif log1_date < log2_date:
+            commands.append(f"DELETE {i+1}")
+            i += 1
+        else:
+            commands.append(f"INSERT [{log2_time}] {logfile2[j][1]}")
+            j += 1
+
+while i < len(logfile1):
+    commands.append(f"DELETE {i+1}")
+    i += 1
+
+while j < len(logfile2):
+    log2_time = logfile2[j][0].lstrip('[').rstrip(']')
+    logfile2[j][1] = logfile2[j][1].replace('  ', ' ')
+    commands.append(f"INSERT [{log2_time}] {logfile2[j][1]}")
+    j += 1
+
+with open('./Milestone3/M3_Output.txt', 'w') as f:
+    for command in commands:
+        f.write(command + "\n")
+
